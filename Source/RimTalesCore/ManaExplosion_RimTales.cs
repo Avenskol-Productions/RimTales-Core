9a9fab0d483c6a3276dfdee26b01d6a43e4e4bd6
AvenkyrPakacit <pwthesnell2017@gmail.com>
2026-08-05 18:14:21 -0500

    Add mana grenades.

---
+++ Source/RimTalesCore/ManaExplosion_RimTales.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace RimTalesCore
+{
+    public class Projectile_ManaExplosion_RimTales : Projectile_Explosive
+    {
+        protected override void Explode()
+        {
+            Map map = this.Map;
+            if (map != null)
+            {
+                float radius = this.def.projectile.explosionRadius;
+                IntVec3 center = this.Position;
+                IEnumerable<Thing> targets = GenRadial.RadialDistinctThingsAround(center, map, radius, true);
+                foreach (Thing thing in targets)
+                {
+                    if (thing is Pawn pawn && pawn.RaceProps.IsFlesh)
+                    {
+                        ApplyManaSickness(pawn);
+                    }
+                }
+            }
+            base.Explode();
+        }
+        private void ApplyManaSickness(Pawn pawn)
+        {
+            HediffDef sicknessDef = DefDatabase<HediffDef>.GetNamedSilentFail("RTC_ManaBuildup");
+            if (sicknessDef != null)
+            {
+                Hediff existingSickness = pawn.health.hediffSet.GetFirstHediffOfDef(sicknessDef);
+                if (existingSickness != null)
+                {
+                    existingSickness.Severity += 0.20f;
+                }
+                else
+                {
+                    Hediff newSickness = HediffMaker.MakeHediff(sicknessDef, pawn, null);
+                    newSickness.Severity = 0.20f;
+                    pawn.health.AddHediff(newSickness, null, null);
+                }
+            }
+        }
+    }
+}
