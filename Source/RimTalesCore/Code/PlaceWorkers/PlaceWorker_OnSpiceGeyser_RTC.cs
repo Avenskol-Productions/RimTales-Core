74b72d6c1d1b49735cd0a87a2c0c73a7cdd67f76
AvenkyrPakacit <pwthesnell2017@gmail.com>
2026-08-26 18:25:54 -0500

    Update to current.

---
+++ Source/RimTalesCore/Code/PlaceWorkers/PlaceWorker_OnSpiceGeyser_RTC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace RimWorld
+{
+    public class PlaceWorker_OnSpiceGeyser_RTC : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            Thing thing2 = map.thingGrid.ThingAt(loc, ThingDefOf_RimTales.RTC_SpiceGeyser);
+            if (thing2 == null || thing2.Position != loc)
+            {
+                return "MustPlaceOnSpiceGeyser".Translate();
+            }
+            return true;
+        }
+        public override bool ForceAllowPlaceOver(BuildableDef otherDef)
+        {
+            return otherDef == ThingDefOf_RimTales.RTC_SpiceGeyser;
+        }
+        public override void DrawMouseAttachments(BuildableDef def)
+        {
+            List<Thing> list = Find.CurrentMap.listerThings.ThingsOfDef(ThingDefOf_RimTales.RTC_SpiceGeyser);
+            for (int i = 0; i < list.Count; i++)
+            {
+                TargetHighlighter.Highlight(list[i], true, true, false);
+            }
+        }
+    }
+}
---
+++ Source/RimTalesCore/Code/ThingDefOf_RimTales.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Verse;
+
+namespace RimWorld
+{
+    // Token: 0x020035A5 RID: 13733
+    [DefOf]
+    public static class ThingDefOf_RimTales
+    {
+        // Token: 0x06013480 RID: 78976 RVA: 0x005BA97D File Offset: 0x005B8B7D
+        static ThingDefOf_RimTales()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(ThingDefOf));
+        }
+        public static ThingDef RTC_SpiceGeyser;
+    }
+}
